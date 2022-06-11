738ad088f25bbe6a668bbdcd489e3fa5650ae1d1
ItaloCovas <italocovas@gmail.com>
2022-06-11 15:13:45 -0300

    Adding loader/improving performance

--- src/App.js
+++ src/App.js
@@ -1,43 +1,64 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Input from './components/Input';
 import Presentation from './components/Presentation';
 import Result from './components/Result';
-import {createWorker} from 'tesseract.js';
+import { createWorker } from 'tesseract.js';
+import Loader from './components/Loader';
 
 
 function App() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [textResult, setTextResult] = useState("");
-  
+
   const worker = createWorker();
 
   const convertImageToText = useCallback(async () => {
-    if(navigator.language === "en-US") {
-      await worker.load();
-      await worker.loadLanguage("eng");
-      await worker.initialize("eng");
-      const { data } = await worker.recognize(selectedImage);
-      await worker.terminate();
-      setTextResult(data.text);
-    }
-    if(navigator.language === "pt-BR") {
-      await worker.load();
-      await worker.loadLanguage("por");
-      await worker.initialize("por");
-      const { data } = await worker.recognize(selectedImage);
-      setTextResult(data.text);
+    switch (navigator.language) {
+      case 'en-US':
+        async function enUsLoader() {
+          setLoading(true);
+          await worker.load();
+          await worker.loadLanguage("eng");
+          await worker.initialize("eng");
+          const { data } = await worker.recognize(selectedImage);
+          setTextResult(data.text);
+          setLoading(false);
+          await worker.terminate();
+        }
+        enUsLoader();
+        break;
+      case 'pt-BR':
+        async function ptBrLoader() {
+          setLoading(true);
+          await worker.load();
+          await worker.loadLanguage("por");
+          await worker.initialize("por");
+          const { data } = await worker.recognize(selectedImage);
+          setTextResult(data.text);
+        }
+        ptBrLoader();
+        break;
+      default:
+        enUsLoader();
+        break;
     }
   }, [worker, selectedImage]);
 
   useEffect(() => {
-    convertImageToText();
-  }, [selectedImage, convertImageToText]);
+    if (selectedImage) {
+      setTextResult("");
+      convertImageToText();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedImage]);
 
   return (
     <div className="App">
       <Presentation />
-      <Input setSelectedImage={setSelectedImage} setTextResult={setTextResult}/>
-      <Result selectedImage={selectedImage} textResult={textResult}/>
+      <Input setSelectedImage={setSelectedImage} setTextResult={setTextResult} />
+      {loading && <Loader />}
+      <Result selectedImage={selectedImage} textResult={textResult} />
     </div>
   );
 }
---
+++ src/components/Loader/index.js
@@ -0,0 +1,9 @@
+import './styles.scss';
+
+export default function Loader() {
+  return (
+    <div className="loader-container">
+    <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+  );
+}
